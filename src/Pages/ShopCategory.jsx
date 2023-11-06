04f083b6df36e305581ea5fcd8dc31650a88d8f6
Rupeshkumar23 <coolrupesh2000@gmail.com>
2023-11-07 02:03:19 +0530

    updated code

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,13 +1,41 @@
-import React, { useContext } from 'react'
-import './CSS/ShopCategory.css'
-import { ShopContext } from '../Context/ShopContext'
+import React, { useContext } from "react";
+import "./CSS/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
+import dropdowm_icon from "../Components/Assets/dropdown_icon.png";
+import Item from "../Components/Item/Item";
 const ShopCategory = (props) => {
-  const {all_product} = useContext(ShopContext);
+  const { all_product } = useContext(ShopContext);
   return (
-    <div className='shop_category'>
-
+    <div className="shop_category">
+      <img src={props.banner} alt="banner" />
+      <div className="shopcategory_indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory_sort">
+          Sort by <img src={dropdowm_icon} alt="dropdowm_icon" />
+        </div>
+      </div>
+      <div className="shopcategory_products">
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShopCategory
+export default ShopCategory;
