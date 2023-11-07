40bc6366f670c9d93bee2415aa37fab6e999a911
Rupeshkumar23 <coolrupesh2000@gmail.com>
2023-11-07 23:44:04 +0530

    Final updated code

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import "./CartItems.css";
 import { ShopContext } from "../../Context/ShopContext";
 import remove_icon from "../Assets/cart_cross_icon.png";
 const CartItems = () => {
-  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  const { all_product, cartItems, removeFromCart,getTotalCartAmount} = useContext(ShopContext);
   return (
     <div className="cartitems">
       <div className="cartitems_format_main">
@@ -20,27 +20,58 @@ const CartItems = () => {
         if (cartItems[e.id] > 0) {
           return (
             <div>
-              <div className="cartitems_format">
+              <div className="cartitems_format cartitems_format_main">
                 <img src={e.image} alt="" className="carticon_product_icon" />
                 <p>{e.name}</p>
                 <p>{e.new_price}</p>
                 <button className="cartitems_quantity">
                   {cartItems[e.id]}
                 </button>
-                <p>{e.new_price * cartItems[e.id]}</p>
+                <p>${e.new_price*cartItems[e.id]}</p>
                 <img
+                className="cartitems_remove_icon"
                   src={remove_icon}
                   onClick={() => {
                     removeFromCart(e.id);
                   }}
-                  alt=""
+                  alt="remove_icon"
                 />
               </div>
               <hr />
             </div>
           );
         }
+        return null;
       })}
+      <div className="cartitems_down">
+        <div className="cartitems_total">
+            <h1>Cart Totals</h1>
+            <div>
+                <div className="cartitems_total_item">
+                    <p>Subtotal</p>
+                    <p>${getTotalCartAmount()}</p>
+                </div>
+                <hr/>
+                <div className="cartitems_total_item">
+                    <p>Shipping Fee</p>
+                    <p>Free</p>
+                </div>
+                <hr/>
+                <div className="cartitems_total_item">
+                    <h3>Total</h3>
+                    <h3>${getTotalCartAmount()}</h3>
+                </div>
+            </div>
+            <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems_promocode">
+            <p>If you have a promo code, Enter it here</p>
+            <div className="cartitems_promobox">
+                <input type="text" placeholder="promo code" />
+                <button>Submit</button>
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
 const Navbar = () => {
 const [menu,setMenu]= useState("shop");
+const {getTotalCartItems}=useContext(ShopContext);
 
   return (
     <div className='navbar'>
@@ -21,7 +23,7 @@ const [menu,setMenu]= useState("shop");
         <div className="nav-login-cart">
          <Link to='/login'> <button>Login</button></Link>
           <Link to='/cart'><img src={cart_icon} alt="add_icon"/></Link>
-          <div className="nav_cart_count">0</div>
+          <div className="nav_cart_count">{getTotalCartItems()}</div>
         </div>
     </div>
   )
